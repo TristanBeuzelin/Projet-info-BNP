90506ec082408089cd1e331d9702674902452572
Hayvo <pierre.denig@mines-paristech.fr>
2021-05-09 15:33:07 +0200

    Average price by product type

---
+++ programms/average_prices.py
@@ -0,0 +1,32 @@
+import pandas as pd 
+import numpy as np 
+import os 
+
+file = "./price_computed/price_filled2.csv"
+
+df = pd.read_csv(file, sep = ";")
+data = df.to_numpy()
+
+header = df.columns
+
+average_price_dict = {}
+
+for line in data:
+    product = line[1].split(' ')[0]
+
+    numbers = np.array([float(i) for i in line[2:]])
+    if product not in average_price_dict.keys():
+        average_price_dict[product] = [1,numbers]
+    else:
+        average_price_dict[product] = [average_price_dict[product][0] + 1, average_price_dict[product][1] +numbers]
+    
+for k in average_price_dict.keys():
+    average_price_dict[k] = [average_price_dict[k][0],np.around(average_price_dict[k][1]/average_price_dict[k][0],2)]
+
+average_price = []
+
+for k in average_price_dict.keys():
+    average_price.append([k] + list(average_price_dict[k][1]))
+
+pd.DataFrame(average_price).to_csv("average_prices.csv",sep=";",header = header[1:])
+
---
+++ programms/merge_data_price.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np 
+import pandas as pd 
+from glob import glob
+import collections
+
+paths = glob(".\data_price/*.csv")
+
+data = {}
+dates = set()
+
+for file in paths:
+    df = pd.read_csv(file, sep=';', engine = 'c',header = 1,usecols= [0,2,5])[:-1]
+    df = df.to_numpy()
+    n = len(df)
+    for i in range(n):
+        date = df[i,0]
+        libele = df[i,1]
+        prix = df[i,2]
+        dates.add(date)
+        if libele in data.keys():
+            data[libele] = data[libele] + [(date,prix)]
+        else:
+            data[libele] = [(date,prix)]
+
+data = collections.OrderedDict(sorted(data.items()))
+dates = list(sorted(dates))
+
+prices = []
+
+
+
+for i,(k,v) in enumerate(data.items()):
+    l = [' \xa0']*len(dates)
+    for x in v:
+        j = dates.index(x[0])
+        l[j] = x[1].replace(',','.')
+    prices.append([k]+l)
+
+pd.DataFrame(prices).to_csv("prices.csv",sep=";",header = ["Produit"] + dates) 
