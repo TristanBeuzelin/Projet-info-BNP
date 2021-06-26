2eb607094915a75e3a956d25e1514b5f2399bb4b
TristanBeuzelin <beuzelin.tri@hotmail.fr>
2021-06-26 19:34:09 +0200

    Gradient boosting adding

--- programms/simulations/tests_crossvalidation.py
+++ programms/simulations/tests_crossvalidation.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np 
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.neural_network import MLPRegressor
+from sklearn.neighbors import KNeighborsRegressor
 import matplotlib.pyplot as plt 
 
 df_price = pd.read_csv("../../price_computed/average_prices.csv",sep = ";") 
@@ -47,8 +48,9 @@ Model = RandomForestRegressor()
 N_list = np.arange(1, 40, 1)
 tuned_parameters = {'n_estimators': N_list}
 nb_folds = 5
-grid = GridSearchCV(Model, tuned_parameters, cv=nb_folds, refit=True, verbose=3)
+grid = GridSearchCV(Model, tuned_parameters, cv=nb_folds, refit=True, verbose=3, scoring='r2')
 grid.fit(prices_learn, IPC_learn.ravel())
+print(grid.best_estimator_)
 scores = grid.cv_results_['mean_test_score']
 scores_std = grid.cv_results_['std_test_score']
 Predict_test = grid.best_estimator_.predict(price_test.T)
@@ -62,7 +64,7 @@ Model = DecisionTreeRegressor()
 depth_list = np.arange(5, 100, 1)
 tuned_parameters = {'max_depth': depth_list}
 nb_folds = 5
-grid = GridSearchCV(Model, tuned_parameters, cv=nb_folds, refit=True, verbose=3, scoring='explained_variance')
+grid = GridSearchCV(Model, tuned_parameters, cv=nb_folds, refit=True, verbose=3, scoring='r2')
 grid.fit(prices_learn, IPC_learn.ravel())
 scores = grid.cv_results_['mean_test_score']
 scores_std = grid.cv_results_['std_test_score']
@@ -77,15 +79,43 @@ Model = MLPRegressor()
 sizes_list = np.arange(50, 100, 5)
 tuned_parameters = {'hidden_layer_sizes': sizes_list}
 nb_folds = 5
-grid = GridSearchCV(Model, tuned_parameters, cv=nb_folds, refit=True, verbose=3, scoring='explained_variance')
+grid = GridSearchCV(Model, tuned_parameters, cv=nb_folds, refit=True, verbose=3, scoring='r2')
 grid.fit(prices_learn, IPC_learn.ravel())
 print(grid.best_estimator_)
 Predict_test = grid.best_estimator_.predict(price_test.T)
 Predict = grid.best_estimator_.predict(prices.T)
 Predict2 = Predict.copy()
 Predict2[-len(Predict_test):] = Predict_test
-IPC_dict = {} #Dictionnary containing each IPCs (values) for each month (keys)
 """
+"""
+# K Neighbors Regression
+Model = KNeighborsRegressor()
+n_list = np.arange(1, 31, 1)
+tuned_parameters = {'n_neighbors': n_list}
+nb_folds = 5
+grid = GridSearchCV(Model, tuned_parameters, cv=nb_folds, refit=True, verbose=3, scoring='r2')
+grid.fit(prices_learn, IPC_learn.ravel())
+print(grid.best_estimator_)
+Predict_test = grid.best_estimator_.predict(price_test.T)
+Predict = grid.best_estimator_.predict(prices.T)
+Predict2 = Predict.copy()
+Predict2[-len(Predict_test):] = Predict_test
+"""
+
+# Gradient Boosting Regression
+Model = GradientBoostingRegressor(learning_rate=0.65, n_estimators=15, max_depth=4, alpha=0.6, tol=0.07)
+n_list = np.arange(0.1, 1, 0.1)
+tuned_parameters = {'validation_fraction': n_list}
+nb_folds = 5
+grid = GridSearchCV(Model, tuned_parameters, cv=nb_folds, refit=True, verbose=3, scoring='r2')
+grid.fit(prices_learn, IPC_learn.ravel())
+print(grid.best_estimator_)
+Predict_test = grid.best_estimator_.predict(price_test.T)
+Predict = grid.best_estimator_.predict(prices.T)
+Predict2 = Predict.copy()
+Predict2[-len(Predict_test):] = Predict_test
+
+IPC_dict = {} #Dictionnary containing each IPCs (values) for each month (keys)
 for [date,ipc] in df_ipc:
     IPC_dict[date] = ipc 
 
